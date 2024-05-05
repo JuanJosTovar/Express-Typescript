26489e0a4831316c3b986561dc58777a877250a0
Juan Jose Tovar <juanjosetovar330@gmail.com>
2024-05-04 19:48:31 -0500

    add metodos getters y setters y reestructuracion de carpetas con nuevo flujo

--- Dto/UserDto.ts
+++ Dto/UserDto.ts
@@ -1,29 +1,101 @@
 class User {
-    email: string;
-    nombres: string;
-    apellidos: string;
-    telefono: string;
-    password: string;
-    edad: number;
-    ciudad: string;
-    pais: string;
-    documento:string;
+    _email: string;
+    _nombres: string;
+    _apellidos: string;
+    _telefono: string;
+    _password: string;
+    _edad: number;
+    _ciudad: string;
+    _pais: string;
+    _documento:string;
     constructor(
         email: string, nombres: string,
         apellidos: string, telefono: string,
         password: string, edad:number, ciudad: string,
         pais: string, documento: string
     ) {
-        this.email = email;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.telefono = telefono;
-        this.password = password;
-        this.edad = edad;
-        this.ciudad = ciudad;
-        this.pais = pais;
-        this.documento = documento;
+        this._email = email;
+        this._nombres = nombres;
+        this._apellidos = apellidos;
+        this._telefono = telefono;
+        this._password = password;
+        this._edad = edad;
+        this._ciudad = ciudad;
+        this._pais = pais;
+        this._documento = documento;
     }
+        //Getters
+        get email(): string{
+            return this._email
+        }
+    
+        get nombres(): string{
+            return this._nombres
+        }
+    
+        get apellidos(): string{
+            return this._apellidos
+        }
+    
+        get telefono(): string{
+            return this._telefono
+        }
+        get password(): string{
+            return this._password
+        }
+        get edad(): number{
+            return this._edad
+        }
+    
+        get ciudad(): string{
+            return this._ciudad
+        }
+
+        get pais(): string{
+            return this._pais
+        }
+
+        get documento(): string{
+            return this._documento
+        }
+    
+        //Setters
+        set email(email:string){
+            this._email = email
+        }
+    
+        set nombres(nombres:string){
+            this._nombres = nombres
+        }
+    
+        set apellidos(apellidos:string){
+            this._apellidos = apellidos
+        }
+    
+        set telefono(telefono:string){
+            this._telefono = telefono
+        }
+    
+        set password(password:string){
+            this._password = password
+        }
+    
+        set edad(edad:number){
+            this._edad = edad
+        }
+    
+        set ciudad(ciudad:string){
+            this._ciudad = ciudad
+        }
+
+        set pais(pais:string){
+            this._pais = pais
+        }
+
+        set documento(documento:string){
+            this._documento = documento
+        }
+        
 }
 
 export default User;
--- controllers/register-controller.ts
+++ controllers/register-controller.ts
@@ -1,7 +1,7 @@
-import bcrypt from 'bcryptjs';
 import UserRepository from '../repositories/UserRepository';
 import User from '../Dto/UserDto';
 import { Request, Response } from "express";
+import UserService from '../services/UserService';
 
 
 let register = async (req: Request, res: Response) => {
@@ -18,11 +18,9 @@ let register = async (req: Request, res: Response) => {
       document
     } = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const result = await UserRepository.add(new User(email, name, lastName, phoneNumber, hashedPassword, age, city, country, document));
+    const result = await UserService.register(new User(email, name, lastName, phoneNumber, password, age, city, country, document));
     return res.status(201).send(
-      { status: 'register ok', password_hasheado: hashedPassword }
+      { status: 'register ok'}
     );
   } catch (error: any) {
     if (error && error.code == "ER_DUP_ENTRY") {
